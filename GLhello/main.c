6cdfb7d5085f1761a633011199e034b07e6caa63
dzqdzq <635587322@qq.com>
2022-04-22 22:19:03 +0800

    Initial Commit

---
+++ GLhello/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GLhello
+//
+//  Created by 邓中强 on 2022/4/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
