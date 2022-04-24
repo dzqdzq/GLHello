4f42888ed1ffa31cb76972a9f2d7d08f7ef07e03
dzqdzq <635587322@qq.com>
2022-04-24 21:15:26 +0800

    绘制矩形

--- GLhello/main.c
+++ GLhello/main.c
@@ -15,11 +15,17 @@ typedef struct Vertex
     vec3 col;
 } Vertex;
 
-static const Vertex vertices[3] =
+static const Vertex vertices[4] =
 {
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+    { { 0.5f,  0.5f }, { 1.f, 0.f, 0.f } },
+    { {  0.5f, -0.5f }, { 0.f, 1.f, 0.f } },
+    { {  -0.5f,  -0.5f }, { 0.f, 0.f, 1.f } },
+    { { -0.5f, 0.5f }, { 1.f, 1.f, 0.f } },
+};
+
+static unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
 };
 
 static const char* vertex_shader_text =
@@ -80,12 +86,17 @@ int main(void)
     glfwSwapInterval(1);
 
     // NOTE: OpenGL error checks have been omitted for brevity
-
-    GLuint vertex_buffer;
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);//第二个参数要设置成你想操作内存的标识，然后接下来的操作都是针对这一块内存进行的
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+ 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
     const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
@@ -103,16 +114,18 @@ int main(void)
     const GLint vpos_location = glGetAttribLocation(program, "vPos");
     const GLint vcol_location = glGetAttribLocation(program, "vCol");
 
-    GLuint vertex_array;
-    glGenVertexArrays(1, &vertex_array);
-    glBindVertexArray(vertex_array);
+//    GLuint vertex_array, idx_vertex_array;
+//    glGenVertexArrays(1, &vertex_array);
+//    glBindVertexArray(vertex_array);
+//    glGenVertexArrays(1, &idx_vertex_array);
+//    glBindVertexArray(idx_vertex_array);
+    
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, pos));
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(Vertex), (void*) offsetof(Vertex, col));
-
     while (!glfwWindowShouldClose(window))
     {
         int width, height;
@@ -127,12 +140,12 @@ int main(void)
         mat4x4_rotate_Z(m, m, (float) glfwGetTime());
         mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
         mat4x4_mul(mvp, p, m);
+//        mat4x4_dup(mvp, m);
 
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
-        glBindVertexArray(vertex_array);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
